0ed8599e223aed7b43b68243046a1af0fe882204
Alyanna Cabrera <alyanna_hazel_cabrera@dlsu.edu.ph>
2024-11-04 19:01:47 +0800

    changed sleep

--- System/SynchronizedClock.h
+++ System/SynchronizedClock.h
@@ -94,7 +94,7 @@ class SynchronizedClock {
 
         void incrementClock() {
             currentSystemClock.store((currentSystemClock.load() + 1) % LLONG_MAX);
-            std::this_thread::sleep_for(std::chrono::milliseconds(0));
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
 
         void turnOff() {
